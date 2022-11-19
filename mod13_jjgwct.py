0e6d41b09ffa81d7f65fafc493adf6340c31cbd7
j0hnthedon <72625213+j0hnthedon@users.noreply.github.com>
2022-11-18 18:15:28 -0600

    Add files via upload

---
+++ mod13_jjgwct.py
@@ -0,0 +1,21 @@
+# Data Visualizer Unit Testing
+
+print("\n Please enter a stock symbol (Capitalized, 1-7 Letters: ")
+test1 = input()
+print(test1)
+
+print("\n Please enter a chart type: For Bar enter 1 and for Chart enter 2")
+test2 = input()
+print(test2)
+
+print("\n Please enter a time series (Select 1-4): Intraday = 1, Daily = 2, Weekly = 3, Monthly = 4")
+test3 = input()
+print(test3)
+
+print("\n Please enter a Start date in the form YYYY-MM-DD")
+test4 = input()
+print(test4)
+
+print("\n Please enter a End date in the form YYYY-MM-DD")
+test5 = input()
+print(test5)
